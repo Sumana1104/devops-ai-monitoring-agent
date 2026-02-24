f8a4462ceb6e4f6a8d813b62d602d89f68d823d4
suman purushotham <sumana.code@gmail.com>
2026-02-24 09:03:17 -0500

    Fix: bind server to 0.0.0.0 for ALB

--- index.js
+++ index.js
@@ -76,6 +76,8 @@ app.get('/orders', (req, res) => {
   ]);
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+
+
 });
