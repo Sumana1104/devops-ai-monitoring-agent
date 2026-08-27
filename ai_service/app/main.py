4ac4770a15bf1655792f370b4d00474fc4c52a39
Sumana1104 <sumana.code@gmail.com>
2026-08-26 20:59:28 -0400

    Update Groq model to mixtral-8x7b-32768

--- ai_service/app/main.py
+++ ai_service/app/main.py
@@ -31,7 +31,7 @@ async def ai_endpoint(request: Request):
 
     try:
         response = groq_client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model="mixtral-8x7b-32768",
             messages=[{"role": "user", "content": user_text}]
         )
         answer = response.choices[0].message.content
